3ad3fc82d39cc9dc72aaaec4b40d065a58aa9fc3
17thaugust <78376364+17thaugust@users.noreply.github.com>
2021-05-09 18:07:12 +0200

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.png';
 import './App.css';
+import Dictionary from './Dictionary.js';
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} alt="logo" className="App-logo" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center">
+        Coded by Karolina
+      </footer>
     </div>
+  </div>
   );
 }
