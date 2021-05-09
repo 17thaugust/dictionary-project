71aac19a8980cc8f50b9c777391503fc48971890
17thaugust <78376364+17thaugust@users.noreply.github.com>
2021-05-09 19:21:10 +0200

    Rendered word meanings

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ export default function App() {
       </header>
       <main>
         <Dictionary />
+        
       </main>
       <footer className="App-footer">
         Coded by Karolina
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
 
 function handleResponse(response) {
-
+setResults(response.data[0]);
 }
 
-    function search (event) {
+function search (event) {
     event.preventDefault();
-
     let apiUrl =  `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
 axios.get(apiUrl).then(handleResponse);
 }
@@ -25,6 +26,8 @@ return <div className="Dictionary">
     <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
     </form>
+    <br />
+    <Results results={results} />
 </div>
 ;
 }
