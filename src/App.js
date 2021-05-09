0f6ebeea9aba2badbb3652e7314428a0b6dc7a68
17thaugust <78376364+17thaugust@users.noreply.github.com>
2021-05-09 21:33:02 +0200

    Debugging

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
     <div className="App">
       <div className="container">
       <header className="App-header">
-        <img src={logo} alt="logo" className="App-logo img-fluid" alt="logo" />
+        <img src={logo} alt="logo" className="App-logo" alt="logo" />
       </header>
       <main>
         <Dictionary defaultKeyword="dusk"/>
